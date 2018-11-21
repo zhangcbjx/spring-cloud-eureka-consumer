9f03a9642364e1c391175cea42c11a6457c1a00f
zhangcb <zhangcb@LAPTOP-FC9BT7S5>
2018-11-21 14:45:35 +0800

    =====

--- src/main/java/com/learn/service/FeignUserFallback.java
+++ src/main/java/com/learn/service/FeignUserFallback.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 /**
  * @ClassName: FeignUserFallback   
- * @Description: 服务容错降级
+ * @Description: 服务容错降级，不能捕获异常信息
  * @author zhangcb  
  * @date 2018年11月21日 下午1:46:53 
  * @Copyright: 2018 www.yunqi.com Inc. All rights reserved.
--- src/main/java/com/learn/service/HystrixClientFallbackFactory.java
+++ src/main/java/com/learn/service/HystrixClientFallbackFactory.java
@@ -6,7 +6,7 @@ import feign.hystrix.FallbackFactory;
 
 /**
  * @ClassName: HystrixClientFallbackFactory   
- * @Description: fallbackFactory与fallback方法不能同时使用,fallbackFactory可以捕捉异常信息
+ * @Description: 服务容错降级 fallbackFactory与fallback方法不能同时使用,fallbackFactory可以捕捉异常信息
  * @author zhangcb  
  * @date 2018年11月21日 下午2:25:49 
  * @Copyright: 2018 www.yunqi.com Inc. All rights reserved.
